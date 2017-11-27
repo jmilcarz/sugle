ad2c512a87defb4b7d16a086e1fbcda473d62791
kmilcarz <j.milcarz@yahoo.com>
2017-11-27 20:17:50 +0100

    frontend done

--- index.php
+++ index.php
@@ -6,20 +6,21 @@
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <meta http-equiv="X-UA-Compatible" content="ie=edge">
      <title>sugle search</title>
-     <link href="https://fonts.googleapis.com/css?family=Barlow+Semi+Condensed|Fira+Sans|Shrikhand" rel="stylesheet">
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+     <link href="https://fonts.googleapis.com/css?family=Barlow+Semi+Condensed:400,600|Fira+Sans|Shrikhand" rel="stylesheet">
      <link rel="stylesheet" href="assets/css/main.css">
 </head>
 <body>
      <div id="searchEngine">
           <?php
                if ($bgRandNumber === 1) {
-                    echo '<img src="assets/img/bg1-small.jpg" alt="" class="background-img">';
+                    echo '<img src="assets/img/bg1-small.jpg" alt="" id="background-img" class="blur">';
                }elseif ($bgRandNumber === 2) {
-                    echo '<img src="assets/img/bg2-small.jpg" alt="" class="background-img">';
+                    echo '<img src="assets/img/bg2-small.jpg" alt="" id="background-img" class="blur">';
                }elseif ($bgRandNumber === 3) {
-                    echo '<img src="assets/img/bg3-small.jpg" alt="" class="background-img">';
+                    echo '<img src="assets/img/bg3-small.jpg" alt="" id="background-img" class="blur">';
                }elseif ($bgRandNumber === 4) {
-                    echo '<img src="assets/img/bg4-small.jpg" alt="" class="background-img">';
+                    echo '<img src="assets/img/bg4-small.jpg" alt="" id="background-img" class="blur">';
                }else {
                     echo "Ooops, something went <b>wrong</b> :/";
                     exit();
@@ -28,6 +29,35 @@
           <div class="search-engine-wrapper">
                <div class="search-engine-logo">
                     <h1>Sugle</h1>
+               </div>
+               <div class="search-engine-form">
+                    <form action="search.php" method="get">
+                         <input type="search" name="q" placeholder="Search sugle, google, wikipedia & web...">
+                    </form>
+                    <ul class="links">
+                         <li><a href="https://www.google.com"><i class="fa fa-google"></i></a></li>
+                         <li><a href="https://www.youtube.com"><i class="fa fa-youtube-play"></i></a></li>
+                         <li><a href="https://www.facebook.com"><i class="fa fa-facebook"></i></a></li>
+                         <li><a href="https://www.twitter.com"><i class="fa fa-twitter"></i></a></li>
+                         <li><a href="https://www.twitch.com"><i class="fa fa-twitch"></i></a></li>
+                         <li><a href="https://www.trello.com"><i class="fa fa-trello"></i></a></li>
+                         <li><a href="https://www.github.com"><i class="fa fa-github"></i></a></li>
+                    </ul>
+                    <div class="info">
+                         <?php
+                         $randHint = rand(1, 4);
+
+                         if ($randHint === 1) {
+                              echo '<p><b>hint!</b> If you want to search <b>wikipedia</b> then at the beginning of the query write <b>"wiki"</b>!</p>';
+                         }elseif ($randHint === 2) {
+                              echo '<p><b>hint!</b> If you want to search <b>sugle database</b> then at the beginning of the query write <b>"sugle"</b>!</p>';
+                         }elseif ($randHint === 3) {
+                              echo '<p><b>hint!</b> If you want to search <b>google</b> then type anything you want!</p>';
+                         }elseif ($randHint === 4) {
+                              echo '<p><b>hint!</b> If you want to search <b>web</b> then type <b>full address</b> starting with <b>www</b>!</p>';
+                         }
+                         ?>
+                    </div>
                </div>
           </div>
      </div>
