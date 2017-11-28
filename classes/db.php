366de659040f81946463cb12ffc300d8f4c0a8e9
Jakub Milcarz <j.milcarz@yahoo.com>
2017-11-28 16:18:28 +0100

    Update db.php

--- classes/db.php
+++ classes/db.php
@@ -1,9 +1,9 @@
 <?php
 class DB {
-     private static $system_dbname = "sh203619_suggle";
-     private static $system_dbpassword = "Kubakuba06";
-     private static $system_dbusername = "sh203619_KubaM";
-     private static $system_dbhost = "mysql6.superhost.pl";
+     private static $system_dbname = "-----";
+     private static $system_dbpassword = "----";
+     private static $system_dbusername = "----";
+     private static $system_dbhost = "-----";
      private static function connect() {
           $pdo = new PDO('mysql:' . self::$system_dbhost . ';dbname=' . self::$system_dbname . ';charset=utf8', '' . self::$system_dbusername . '', '' . self::$system_dbpassword . '');
           $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
